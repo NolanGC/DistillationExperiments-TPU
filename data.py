63b8636bd41b68777744e6e5b5fa4ca588698c97
Nolan Clement <nolanclement@dhcp-10-31-45-57.dyn.MIT.EDU>
2022-12-17 14:41:01 -0500

    loading data

---
+++ data.py
@@ -0,0 +1,20 @@
+from torchvision.transforms import ToTensor, Normalize, RandomHorizontalFlip, RandomCrop
+
+def get_dataset():
+    min_vals = (0.0,0.0,0.0)
+    max_vals = (1.0,1.0,1.0)
+    offset = [0.5 * (min_val + max_val) for min_val, max_val in zip(min_vals, max_vals)]
+    scale = [(max_val - min_val) / 2 for max_val, min_val in zip(max_vals, min_vals)]
+    
+    transforms = torchvision.transforms.Compose(
+        [
+            ToTensor(),
+            Normalize(offset, scale),
+            RandomHorizontalFlip(p=0.5),
+            RandomCrop(size=32, padding=4),
+            
+        ]
+    )
+    train_dataset = torchvision.datasets.CIFAR100(root=dataset_dir, train=True, download=True, transform=transforms)
+    test_dataset = torchvision.datasets.CIFAR100(root=dataset_dir, train=False, download=True, transform=transforms)
+    return train_dataset, test_dataset
--- main.py
+++ main.py
@@ -15,13 +15,63 @@ import torch_xla.distributed.parallel_loader as pl
 import torch_xla.distributed.xla_multiprocessing as xmp
 import torch_xla.utils.utils as xu
 import torchvision
+import argparse
 from torchvision import datasets, transforms
 # ---------------------------------------------------------------------------- #
 #                                module imports                                #
 # ---------------------------------------------------------------------------- #
 from models import PreResnet
+from data import get_dataset
+# ---------------------------------------------------------------------------- #
+#                                   CLI args                                   #
+# ---------------------------------------------------------------------------- #
+parser = argparse.ArgumentParser()
+parser.add_argument('--permuted', action='store_true', help='permuted argument')
+parser.add_argument('--loadTeachers', action='store_true', help='load teachers from state dict argument')
+parser.add_argument("-d", "--directory", required=True, help="directory path")
+args = parser.parse_args()
+dataset_dir = 'data/datasets'
+output_dir = args.directory
+# ---------------------------------------------------------------------------- #
+#                               experiment flags                               #
+# ---------------------------------------------------------------------------- #
+FLAGS = {}
+FLAGS['batch_size'] = 128
+FLAGS['num_workers'] = 4 #TODO from XLA example, verify this number is optimal
+FLAGS['learning_rate'] = 5e-2
+FLAGS['momentum'] = 0.9
+FLAGS['weight_decay'] = 1e-4
+FLAGS['nestrov'] = True
+FLAGS['teacher_epochs'] = 200
+FLAGS['ensemble_size'] = 3
 
+SERIAL_EXEC = xmp.MpSerialExecutor()
+WRAPPED_MODEL = xmp.MpModelWrapper(PreResnet(depth=56))
 
+train_dataset, test_dataset = SERIAL_EXEC.run(get_dataset)
+train_sampler = torch.utils.data.distributed.DistributedSampler(
+      train_dataset,
+      num_replicas=xm.xrt_world_size(),
+      rank=xm.get_ordinal(),
+      shuffle=True)
+train_loader = torch.utils.data.DataLoader(
+      train_dataset,
+      batch_size=FLAGS['batch_size'],
+      sampler=train_sampler,
+      num_workers=FLAGS['num_workers'],
+      drop_last=True)
+test_loader = torch.utils.data.DataLoader(
+      test_dataset,
+      batch_size=FLAGS['batch_size'],
+      shuffle=False,
+      num_workers=FLAGS['num_workers'],
+      drop_last=True)
 
-
+print('train_loader length: ', len(train_loader))
+print('test_loader length: ', len(test_loader))
+#learning_rate = FLAGS['learning_rate'] * xm.xrt_world_size()
+#device = xm.xla_device()
+#model = WRAPPED_MODEL.to(device)
+#optimizer = optim.SGD(model.parameters(), lr=learning_rate,
+#                      momentum=FLAGS['momentum'], weight_decay=5e-4)
 
