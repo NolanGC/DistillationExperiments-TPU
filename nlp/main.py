104dfb27f8c1d720bb27a452ecfeb1db8e40ee38
Tian Jin <tianjin@mit.edu>
2023-01-03 21:58:27 +0000

    Make things distributed.

--- nlp/main.py
+++ nlp/main.py
@@ -1,4 +1,7 @@
-import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.parallel_loader as pl
+import torch_xla.distributed.xla_multiprocessing as xmp
+
 import torch
 device='cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -10,61 +13,83 @@ from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from transformers import Trainer, TrainingArguments
 from datasets import load_dataset,load_metric
 
-train_dataset = load_dataset('glue', 'sst2', split='train')
-val_dataset = load_dataset('glue', 'sst2', split='validation')
-test_dataset = load_dataset('glue', 'sst2', split='test')
-train_dataset = train_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
-val_dataset = val_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
-test_dataset = test_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
-val_dataset = val_dataset.remove_columns(['label'])
-test_dataset = test_dataset.remove_columns(['label'])
-train_dataset = train_dataset.remove_columns(['label'])
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-MAX_LENGTH = 128
-train_dataset = train_dataset.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=MAX_LENGTH), batched=True)
-val_dataset = val_dataset.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=MAX_LENGTH), batched=True)
-test_dataset = test_dataset.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=MAX_LENGTH), batched=True)
-train_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
-val_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
-test_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
-def compute_metrics(pred):
-    labels = pred.label_ids
-    preds = pred.predictions.argmax(-1)
-    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='macro')
-    acc = accuracy_score(labels, preds)
-    return {
-        'accuracy': acc,
-        'f1': f1,
-        'precision': precision,
-        'recall': recall
-    }
+import contextlib
+import io
+import sys
 
-#start training
-training_args = TrainingArguments(
-    output_dir='./results',          #output directory
-    learning_rate=1e-4,
-    num_train_epochs=5,
-    per_device_train_batch_size=32,                #batch size per device during training
-    per_device_eval_batch_size=32,                #batch size for evaluation
-    logging_dir='./logs',
-    logging_steps=100,
-    do_train=True,
-    do_eval=True,
-    no_cuda=False,
-    load_best_model_at_end=True,
-    # eval_steps=100,
-    evaluation_strategy="epoch"
-)
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    save_stderr = sys.stderr
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=val_dataset,
-    compute_metrics=compute_metrics
-)
+    sys.stdout = io.BytesIO()
+    sys.stderr = io.BytesIO()
+    yield
+    sys.stdout = save_stdout
+    sys.stderr = save_stderr
+    
+def _mp_fn(index):
+    with nostdout():
+        train_dataset = load_dataset('glue', 'sst2', split='train')
+        val_dataset = load_dataset('glue', 'sst2', split='validation')
+        test_dataset = load_dataset('glue', 'sst2', split='test')
+        train_dataset = train_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
+        val_dataset = val_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
+        test_dataset = test_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
+        val_dataset = val_dataset.remove_columns(['label'])
+        test_dataset = test_dataset.remove_columns(['label'])
+        train_dataset = train_dataset.remove_columns(['label'])
+        model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        MAX_LENGTH = 128
+        train_dataset = train_dataset.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=MAX_LENGTH), batched=True)
+        val_dataset = val_dataset.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=MAX_LENGTH), batched=True)
+        test_dataset = test_dataset.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=MAX_LENGTH), batched=True)
+        train_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
+        val_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
+        test_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
 
-train_out = trainer.train()
-torch.save(model.state_dict(), './sst2_teacher_model.pt')
+    def compute_metrics(pred):
+        labels = pred.label_ids
+        preds = pred.predictions.argmax(-1)
+        precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='macro')
+        acc = accuracy_score(labels, preds)
+        return {
+            'accuracy': acc,
+            'f1': f1,
+            'precision': precision,
+            'recall': recall
+        }
 
+    #start training
+    training_args = TrainingArguments(
+        output_dir='./results',          #output directory
+        learning_rate=1e-4,
+        num_train_epochs=5,
+        per_device_train_batch_size=32,                #batch size per device during training
+        per_device_eval_batch_size=32,                #batch size for evaluation
+        logging_dir='./logs',
+        logging_steps=100,
+        do_train=True,
+        do_eval=True,
+        no_cuda=False,
+        load_best_model_at_end=True,
+        # eval_steps=100,
+        evaluation_strategy="epoch"
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        compute_metrics=compute_metrics
+    )
+
+    xm.rendezvous("training_start")
+
+    train_out = trainer.train()
+    torch.save(model.state_dict(), './sst2_teacher_model.pt')
+
+if __name__ == '__main__':
+    xmp.spawn(_mp_fn, args=())
