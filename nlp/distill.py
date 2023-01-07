4a180d4f1255f3199eadc411b130e8d9bfc2c8e4
Tian Jin <tianjin@mit.edu>
2023-01-07 01:49:30 +0000

    Checkpoint.

--- nlp/distill.py
+++ nlp/distill.py
@@ -15,27 +15,42 @@ from transformers import get_linear_schedule_with_warmup
 from datasets import load_dataset,load_metric
 
 def _mp_fn(index):
-    device = xm.xla_device()
-
     train_dataset = load_dataset('glue', 'sst2', split='train')
     val_dataset = load_dataset('glue', 'sst2', split='validation')
     test_dataset = load_dataset('glue', 'sst2', split='test')
 
+    train_dataset = train_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
+    val_dataset = val_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
+    test_dataset = test_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
+
+    val_dataset = val_dataset.remove_columns(['label'])
+    test_dataset = test_dataset.remove_columns(['label'])
+    train_dataset = train_dataset.remove_columns(['label'])
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    MAX_LENGTH = 128
+    train_dataset = train_dataset.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=MAX_LENGTH), batched=True)
+    val_dataset = val_dataset.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=MAX_LENGTH), batched=True)
+    test_dataset = test_dataset.map(lambda e: tokenizer(e['sentence'], truncation=True, padding='max_length', max_length=MAX_LENGTH), batched=True)
+    train_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
+    val_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
+    test_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
     train_dataloader = DataLoader(train_dataset, sampler=RandomSampler(train_dataset), batch_size=32) #prepare dataloader
 
-    bert_config_T3 = BertConfig.from_json_file('./TextBrewer/examples/student_config/bert_base_cased_config/bert_config_L3.json')
+    bert_config_T3 = BertConfig.from_json_file('./bert_config_L3.json')
     bert_config_T3.output_hidden_states = True
+
+    device = xm.xla_device()
 
     student_model = BertForSequenceClassification(bert_config_T3) #, num_labels = 2
     student_model.to(device=device)
- 
 
-    bert_config = BertConfig.from_json_file('./TextBrewer/examples/student_config/bert_base_cased_config/bert_config.json')
+    bert_config = BertConfig.from_json_file('./bert_config.json')
     bert_config.output_hidden_states = True
     teacher_model = BertForSequenceClassification(bert_config) #, num_labels = 2
     teacher_model.load_state_dict(Platform.load_model('gs://tianjin-distgen/sst2_teacher_model.pt', map_location=torch.device('cpu')))
     teacher_model.to(device=device)
-    
+
     num_epochs = 20
     num_training_steps = len(train_dataloader) * num_epochs
     # Optimizer and learning rate scheduler
@@ -50,20 +65,20 @@ def _mp_fn(index):
         return {'logits': model_outputs.logits, 'hidden': model_outputs.hidden_states}
 
     distill_config = DistillationConfig(
-        intermediate_matches=[    
+        intermediate_matches=[
          {'layer_T':0, 'layer_S':0, 'feature':'hidden', 'loss': 'hidden_mse','weight' : 1},
          {'layer_T':8, 'layer_S':2, 'feature':'hidden', 'loss': 'hidden_mse','weight' : 1}])
     train_config = TrainingConfig(device=device)
 
     distiller = GeneralDistiller(
         train_config=train_config, distill_config=distill_config,
-        model_T=teacher_model, model_S=student_model, 
+        model_T=teacher_model, model_S=student_model,
         adaptor_T=simple_adaptor, adaptor_S=simple_adaptor)
 
 
     with distiller:
-        distiller.train(optimizer, train_dataloader, num_epochs, 
-            scheduler_class=scheduler_class, 
+        distiller.train(optimizer, train_dataloader, num_epochs,
+            scheduler_class=scheduler_class,
             scheduler_args = scheduler_args, callback=None)
 
     # xm.rendezvous("training_start")
