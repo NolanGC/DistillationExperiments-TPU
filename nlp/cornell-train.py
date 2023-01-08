29d865869c1c12780b091791557d8e47780eebb6
Tian Jin <tianjin@mit.edu>
2023-01-08 20:51:44 +0000

    train/eval working.

--- nlp/cornell-train.py
+++ nlp/cornell-train.py
@@ -375,7 +375,7 @@ def _mp_fn(index, args):
             global_step = checkpoint.split("-")[-1] if len(checkpoints) > 1 else ""
             model = model_class.from_pretrained(checkpoint)
             model.to(args.device)
-            result, _ = evaluate(args, model, tokenizer, labels, pad_token_label_id, mode="dev", prefix=global_step)
+            result, _ = evaluate(args, model, tokenizer, labels, pad_token_label_id, mode="test", prefix=global_step)
             if global_step:
                 result = {"{}_{}".format(global_step, k): v for k, v in result.items()}
             results.update(result)
@@ -384,31 +384,31 @@ def _mp_fn(index, args):
             for key in sorted(results.keys()):
                 writer.write("{} = {}\n".format(key, str(results[key])))
 
-    if args.do_predict and args.local_rank in [-1, 0]:
-        tokenizer = tokenizer_class.from_pretrained(args.output_dir, do_lower_case=args.do_lower_case)
-        model = model_class.from_pretrained(args.output_dir)
-        model.to(args.device)
-        result, predictions = evaluate(args, model, tokenizer, labels, pad_token_label_id, mode="test")
-        # Save results
-        output_test_results_file = os.path.join(args.output_dir, "test_results.txt")
-        with open(output_test_results_file, "w") as writer:
-            for key in sorted(result.keys()):
-                writer.write("{} = {}\n".format(key, str(result[key])))
-        # Save predictions
-        output_test_predictions_file = os.path.join(args.output_dir, "test_predictions.txt")
-        with open(output_test_predictions_file, "w") as writer:
-            with open(os.path.join(args.data_dir, "test.txt"), "r") as f:
-                example_id = 0
-                for line in f:
-                    if line.startswith("-DOCSTART-") or line == "" or line == "\n":
-                        writer.write(line)
-                        if not predictions[example_id]:
-                            example_id += 1
-                    elif predictions[example_id]:
-                        output_line = line.split()[0] + " " + predictions[example_id].pop(0) + "\n"
-                        writer.write(output_line)
-                    else:
-                        logger.warning("Maximum sequence length exceeded: No prediction for '%s'.", line.split()[0])
+    # if args.do_predict and args.local_rank in [-1, 0]:
+    #     tokenizer = tokenizer_class.from_pretrained(args.output_dir, do_lower_case=args.do_lower_case)
+    #     model = model_class.from_pretrained(args.output_dir)
+    #     model.to(args.device)
+    #     result, predictions = evaluate(args, model, tokenizer, labels, pad_token_label_id, mode="test")
+    #     # Save results
+    #     output_test_results_file = os.path.join(args.output_dir, "test_results.txt")
+    #     with open(output_test_results_file, "w") as writer:
+    #         for key in sorted(result.keys()):
+    #             writer.write("{} = {}\n".format(key, str(result[key])))
+    #     # Save predictions
+    #     output_test_predictions_file = os.path.join(args.output_dir, "test_predictions.txt")
+    #     with open(output_test_predictions_file, "w") as writer:
+    #         with open(os.path.join(args.data_dir, "test.txt"), "r") as f:
+    #             example_id = 0
+    #             for line in f:
+    #                 if line.startswith("-DOCSTART-") or line == "" or line == "\n":
+    #                     writer.write(line)
+    #                     if not predictions[example_id]:
+    #                         example_id += 1
+    #                 elif predictions[example_id]:
+    #                     output_line = line.split()[0] + " " + predictions[example_id].pop(0) + "\n"
+    #                     writer.write(output_line)
+    #                 else:
+    #                     logger.warning("Maximum sequence length exceeded: No prediction for '%s'.", line.split()[0])
 
     return results
 
