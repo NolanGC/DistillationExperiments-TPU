b5f2e6b17d83be68328173bf2fd0089c91474c27
Tian Jin <tianjin@mit.edu>
2023-01-10 05:36:34 +0000

    check in training script.

--- nlp/cornell.sh
+++ nlp/cornell.sh
@@ -1,3 +1,27 @@
+############ Baseline T3 Directly Trained #################
+# export OUTPUT_DIR=conll-t3-train-outputs-model-base
+# export BATCH_SIZE=32
+# export NUM_EPOCHS=60.
+# export SAVE_STEPS=750
+# export SEED=42
+# export MAX_LENGTH=128
+# export BERT_MODEL=bert-base-cased
+# python3 -u cornell-train.py \
+# --data_dir conll2003 \
+# --model_type bert \
+# --model_name_or_path $BERT_MODEL \
+# --output_dir $OUTPUT_DIR \
+# --max_seq_length  $MAX_LENGTH \
+# --num_train_epochs $NUM_EPOCHS \
+# --per_gpu_train_batch_size $BATCH_SIZE \
+# --per_gpu_eval_batch_size $BATCH_SIZE \
+# --save_steps $SAVE_STEPS \
+# --seed $SEED \
+# --num_hidden_layers 3 \
+# --do_train \
+# --do_eval \
+# --do_predict
+
 # export OUTPUT_DIR=conll-outputs-model-base
 # export BATCH_SIZE=32
 # export NUM_EPOCHS=60.
